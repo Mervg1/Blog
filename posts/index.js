16f33e467b6d32d300db0daa1a99beb72a3ed123
Mercedes Vázquez García <a01655224@itesm.mx>
2023-02-21 20:50:06 -0600

    comments service up and running

--- posts/index.js
+++ posts/index.js
@@ -15,9 +15,10 @@ app.get('/posts', (req, res) => {
 app.post('/posts', (req, res) => {
     const id = randomBytes(4).toString('hex');
     const {title} = req.body;
-
+    
     posts[id] = {
-        id, title
+        id, 
+        title
     };
 
     res.status(201).send(posts[id]);
