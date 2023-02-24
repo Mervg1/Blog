430435fddb3825a84b8d3decc80e3ace79f0faf7
Mercedes Vázquez García <a01655224@itesm.mx>
2023-02-23 22:59:18 -0600

    Comments creation

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import CommentCreate from './CommentCreate';
+import CommentList from './CommentList';
 
 export default () => {
     const [posts, setPosts] = useState({});
@@ -20,6 +22,8 @@ export default () => {
         key={post.id}>
             <div className='card-body'>
                 <h3>{post.title}</h3>
+                <CommentList postId={post.id}></CommentList>
+                <CommentCreate postId={post.id}></CommentCreate>
             </div>
         </div>
     })
