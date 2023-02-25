d418c8073d1c0e13ef6eefebb1bd784d7d300a74
Mercedes Vázquez García <a01655224@itesm.mx>
2023-02-25 12:54:23 -0600

    event bus now stores info, to process al comment moderations even if it was down on time of moderation time

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -4,9 +4,24 @@ export default({comments}) => {
    
 
     const renderComments = comments.map(comment => {
+        
+        let content;
+
+        if(comment.status === 'approved'){
+            content = comment.content;
+        }
+
+        if(comment.status === 'pending'){
+            content = 'This comment is awaiting moderation';
+        }
+
+        if(comment.status === 'rejected'){
+            content = 'This comment has been rejected';
+        }
+
         return (
             <li key={comment.id}>
-                {comment.content}
+                {content}
             </li>
         );
     });
